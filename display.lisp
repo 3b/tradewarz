620f1502ed116b4d6d6110a4f569f5db2241afb3
Michael Fiano <michael.fiano@gmail.com>
2014-11-21 22:51:57 -0500

    minor changes to display init

--- display.lisp
+++ display.lisp
@@ -15,22 +15,19 @@
 (defmethod initialize-instance :after ((object display) &key)
   (sdl:window (width object)
               (height object)
-              :flags sdl:sdl-opengl
-              :resizable t
-              :double-buffer t
               :title-caption (title object)
-              :icon-caption (title object))
+              :icon-caption (title object)
+              :opengl t
+              :opengl-attributes '((:SDL-GL-DOUBLEBUFFER 1))
+              :fps (make-instance 'sdl:fps-fixed
+                                  :target-frame-rate (fps object)))
   (configure object))
 
 (defmethod configure ((object display))
-  (setf cl-opengl-bindings:*gl-get-proc-address*
-        #'sdl-cffi::sdl-gl-get-proc-address
-        (sdl:frame-rate) (fps object))
   (sdl:enable-key-repeat 1 20)
+  (sdl:show-cursor nil)
   (gl:viewport 0 0 (width object) (height object))
   (gl:matrix-mode :projection)
-  (gl:load-identity)
-  (gl:matrix-mode :modelview)
   (gl:load-identity)
   (glu:perspective 60.0 (/ (width object) (height object)) 1 1000.0)
   (glu:look-at 0 150 -300 0 0 0 0 0 -1)
