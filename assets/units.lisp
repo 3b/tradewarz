b38ec391152c7605c1184e76001dad2d65b8c0df
Bart Botta <00003b@gmail.com>
2014-12-26 19:08:05 -0600

    add helicopter models

--- assets/units.lisp
+++ assets/units.lisp
@@ -8,4 +8,20 @@
    (:primitive :triangles
     :object "jet"
     :size (12 12 12)))
+
+ (:heli
+   (:primitive :triangles
+    :object "heli"
+    :size (12 12 12)))
+
+ (:heli-rotor-top
+   (:primitive :triangles
+    :object "heli-rotor-top"
+    :size (12 12 12)))
+
+ (:heli-rotor-tail
+   (:primitive :triangles
+    :object "heli-rotor-tail"
+    :size (12 12 12)))
+
 )
